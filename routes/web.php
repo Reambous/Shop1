8fab1039301ba29b81ad3fb723150312b3dca107
MHaidar <muhammadhaidar159@gmail.com>
2025-12-11 07:57:05 +0700

    persiapan tailwind, dll

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', ['nama' => 'Haidarrr']);
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
